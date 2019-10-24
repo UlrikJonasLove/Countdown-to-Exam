bd2a038401869c4537e6656b02d915f4150ec519
joro1993 <54711342+joro1993@users.noreply.github.com>
2019-10-24 18:49:54 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date ("may 28, 2021 17:00:00").getTime();
+
+
+var x = setInterval(function(){
+var now = new Date().getTime();
+var distance = countDownDate - now;
+
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// visa resultaten i elementet med id "para"
+document.getElementById("para").innerHTML = days + " Dagar " + hours + " Timmar " + minutes + " Minuter " + seconds + " Sekunder ";
+
+//när countdown är klar skriv ut
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("para").innerHTML = "Om du läser detta, gratis! Du klarade det. Du är nu examinerad .NET webbutvecklare!!! GRATTIS!";
+}
+}, 1000);
